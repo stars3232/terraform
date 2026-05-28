870c0e64fbaa16042d06ebc47dec831194968a3e
siva <sivasravan32@gmail.com>
2026-05-28 17:58:17 +0530

    terraform practice

---
+++ ec2/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic from all  ip addresses"
+  
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
