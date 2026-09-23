e68d4db930910bdcfe9d20702abb20a86475a1e2
siva <sivasravan32@gmail.com>
2026-09-23 20:51:57 +0530

    terraform practice

--- loops/r53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "www" {
-  count   = length(var.instances)
-  zone_id = var.zone_id
-  name    = "${var.instances[count.index]}.${var.domain_name}"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.roboshop[count.index].private_ip]
-}
